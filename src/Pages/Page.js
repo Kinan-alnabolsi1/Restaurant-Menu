8dc722000afd287e76ed0bbb086cdf5c87a9e93f
kinan-alnabolsi <kinanalnablsi1@gmail.com>
2025-06-17 02:36:59 +0300

    add logo

--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -1,10 +1,12 @@
 import Swipe from '../Components/Swipe'
 import FoodFilter from '../Components/FoodFilter'
-
+import logo from '../assets/anteka_logo.png'
 const Page = () => {
   return (
     <div>
-        <div className='flex justify-center items-center text-5xl font-bold py-5 text-[#facc15]'>LOGO</div>
+        <div className='flex justify-center items-center font-bold py-5 '>
+          <img src={logo} alt="LOGO" className='w-32 h-auto'/>
+        </div>
             <div><Swipe/></div>
             <div><FoodFilter/></div>
     </div>
